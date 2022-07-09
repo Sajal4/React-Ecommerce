d4a866b13314142dd1c50d7cfac673e998316620
Sajal Debnath <94454173+Sajal4@users.noreply.github.com>
2022-07-09 16:03:28 +0530

    added ecommerce

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+import Navigationbar from './components/Navigationbar'
+import Home from './components/Home'
+import Cart from './components/Cart'
+import CheckOutForm from './components/CheckOutForm'
+import  Favorites from './components/Favorites'
+import OrderPlaced from './components/OrderPlaced'
+
+export class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <BrowserRouter>
+        <Navigationbar /> 
+          
+          <Routes>
+            <Route path="/success" element={<OrderPlaced />}></Route>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route  path="/cart" element={<Cart />}></Route>
+            <Route  path="/recipe" element={<CheckOutForm />}></Route>
+            <Route exact path="/favorites" element={<Favorites />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    )
+  }
+}
+
+export default App
